2d9abed205c36e1d37dce531300addde3f912831
Eliazar Agwaya <eliazar.magero@student.moringaschool.com>
2022-06-22 06:58:46 +0300

    Writes the click functionality

--- assets/main.js
+++ assets/main.js
@@ -1,2 +1,10 @@
-const btn=document.getElementById("head")
-console.log(btn)
+
+const btn=document.getElementById("btn")
+const welcome=(e) => {
+e.preventDefault()
+const input=document.querySelector("input").value
+const element= document.createElement("p");
+element.innerHTML=`Hello ${input}, welcome to the bootcamp`
+document.body.appendChild(element)
+}
+btn.addEventListener("click", welcome)
